17a2c075e8d105bae4f2cfad180dd56c8cd98a07
Gabriel Mahé <mahegabriel@gmail.com>
2024-12-27 15:30:09 +0100

    Day07 :white_check_mark:

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,54 +1,52 @@
-import kotlin.math.abs
-
-fun List<Int>.solverTimes(testValue: Int) = reduce { acc, value ->
-    if (acc * value > testValue) acc + value
-    else acc * value
-}
-
-fun List<Int>.solver(testValue: Int) = reduceIndexed { index, acc, value ->
-    val sum = subList(index, size).sum()
-    val mult = subList(index, size).reduce { tot, i -> tot * i  }
-    mapOf(
-        Pair(abs(acc + sum - testValue), "+"),
-        Pair(abs(acc * sum - testValue), "*"),
-        Pair(abs(acc * mult - testValue), "*"),
-        Pair(abs(acc + mult - testValue), "+")
-    )
-        .also { it.println() }
-        .minBy { it.key }
-        .also { println("$acc${it.value}$value") }
-        .let {
-            when(it.value) {
-                "+" -> acc + value
-                "*" -> acc * value
+private fun List<Long>.testSolutionBin(testValue: Long) = IntRange(0, "1".repeat(lastIndex).toInt(2))
+    .map { it.toString(2).padStart(lastIndex, '0') }
+    .any { binary ->
+        reduceIndexed { index, acc, long ->
+            when(binary[index - 1]) {
+                '0' -> acc + long
+                '1' -> acc * long
                 else -> throw Exception("not possible")
             }
-        }
-        .also { it.println() }
-}
-
-fun List<Int>.solverBis(testValue: Int) = reduceIndexed { index, acc, value ->
-    subList(index, size).reduce { mult, i -> mult*i }.let { allmult ->
-        println("$acc * $allmult ? $testValue")
-        if (acc*allmult > testValue) acc + value
-        else acc * value
+        } == testValue
     }
-    if (acc * subList(index, size).reduce { it, i -> it*i } > testValue) acc + value
-    else acc * value
-}
+
+private fun List<Long>.testSolutionTer(testValue: Long) = LongRange(0, "2".repeat(lastIndex).toLong(3))
+    .map { it.toString(3).padStart(lastIndex, '0') }
+    .any { ternary ->
+        reduceIndexed { index, acc, long ->
+            when(ternary[index - 1]) {
+                '0' -> acc + long
+                '1' -> acc * long
+                '2' -> (acc.toString() + long.toString()).toLong()
+                else -> throw Exception("not possible")
+            }
+        } == testValue
+    }
 
 fun main() {
-    fun part1(input: List<String>): Int = input.sumOf { line ->
-        val (testValueStr, valuesStr) = line.split(": ")
-        val testValue = testValueStr.toInt()
-        val values = valuesStr.split(" ").map { it.toInt() }
+    fun part1(input: List<String>) = input.sumOf { line ->
+        line.split(": ").let { (testValueStr, valuesStr) ->
+            val testValue = testValueStr.toLong()
+            val values = valuesStr.split(" ").map { it.toLong() }
 
-        testValue
+            if (values.testSolutionBin(testValue)) testValue else 0
+        }
     }
 
-    fun part2(input: List<String>): Int = input.size
+    fun part2(input: List<String>) = input.sumOf { line ->
+        line.split(": ").let { (testValueStr, valuesStr) ->
+            val testValue = testValueStr.toLong()
+            val values = valuesStr.split(" ").map { it.toLong() }
 
-    val input = readInput("test")
+            if (values.testSolutionTer(testValue)) testValue else 0
+        }
+    }
+
+    val test = readInput("test")
+    val input = readInput("Day07")
+    part1(test).println()
     part1(input).println()
-    part2(input).println()
+    part2(test).println()
+//    not optimized !
+//    part2(input).println()
 }
